4467343cb253e06aed140b970fa5b8d16f7c6935
Ervin Rivas <sa8ere5@gmail.com>
2021-09-17 15:12:47 -0500

    incremento de recursos del systema para calculos

--- Python OOP/busqueda_binaria.py
+++ Python OOP/busqueda_binaria.py
@@ -1,35 +1,29 @@
 import random
+import sys
+#print(sys.getrecursionlimit())
+sys.setrecursionlimit(11000000)
+#print(sys.getrecursionlimit())
 
-class BusquedaBinaria:
-
-    def __init__(self, lista, comienzo, final, objetivo):
-        self.lista = lista
-        self.comienzo = comienzo
-        self.final = final
-        self.objetivo = objetivo
-
-    def busqueda_binaria(self):
+def busqueda_binaria(lista, comienzo, final, objetivo):
         
-        if self.comienzo > self.final:
-            return False
+    if comienzo > final:
+        return False
 
-        medio = (self.comienzo +self.final) // 2
+    medio = ((comienzo + final) // 2)
 
-        if lista[medio] == self.objetivo:
-            return True
-        elif lista[medio] < self.objetivo:
-            temp1 = BusquedaBinaria(lista, medio + 1 ,self.final, self.objetivo)
-            return temp1.busqueda_binaria()
-        else:
-            temp2 = BusquedaBinaria(lista, medio - 1 ,self.final, self.objetivo)
-            return temp2.busqueda_binaria()
+    if lista[medio] == objetivo:
+        return True
+    elif lista[medio] < objetivo:
+        return busqueda_binaria(lista, medio+1, final, objetivo)
+        
+    else:
+        return busqueda_binaria(lista, medio-1, final, objetivo)
+        
 
-    if __name__ == '__main__':
-        pass
-
-list_size = int(input('De que tamano sera la lista? '))
-objetivo = int(input("Que numero quieres encontra? "))
-lista = sorted([random.randint(0, 100) for i in range(list_size)])
-busqueda = BusquedaBinaria(lista, 0, len(lista), objetivo)
-encontrado = busqueda.busqueda_binaria()
-print(f'El elemento  {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+if __name__ == '__main__':
+    list_size = int(input('De que tamano sera la lista? '))
+    objetivo = int(input("Que numero quieres encontra? "))
+    lista = sorted([random.randint(0, 100) for i in range(list_size)])
+    print(lista)
+    encontrado = busqueda_binaria(lista, 0, len(lista), objetivo)
+    print(f'El elemento  {objetivo} {"esta" if encontrado else "no esta"} en la lista')
---
+++ Python OOP/busqueda_binaria_poo.py
@@ -0,0 +1,37 @@
+import random
+import sys
+sys.setrecursionlimit(99000000)
+
+class BusquedaBinaria:
+
+    def __init__(self, lista, comienzo, final, objetivo):
+        self.lista = lista
+        self.comienzo = comienzo
+        self.final = final
+        self.objetivo = objetivo
+
+    def busqueda_binaria(self):
+        
+        if self.comienzo > self.final:
+            return False
+
+        medio = (self.comienzo +self.final) // 2
+
+        if lista[medio] == self.objetivo:
+            return True
+        elif lista[medio] < self.objetivo:
+            temp1 = BusquedaBinaria(lista, medio + 1 ,self.final, self.objetivo)
+            return temp1.busqueda_binaria()
+        else:
+            temp2 = BusquedaBinaria(lista, medio - 1 ,self.final, self.objetivo)
+            return temp2.busqueda_binaria()
+
+    if __name__ == '__main__':
+        pass
+
+list_size = int(input('De que tamano sera la lista? '))
+objetivo = int(input("Que numero quieres encontra? "))
+lista = sorted([random.randint(0, 100) for i in range(list_size)])
+busqueda = BusquedaBinaria(lista, 0, len(lista), objetivo)
+encontrado = busqueda.busqueda_binaria()
+print(f'El elemento  {objetivo} {"esta" if encontrado else "no esta"} en la lista')
